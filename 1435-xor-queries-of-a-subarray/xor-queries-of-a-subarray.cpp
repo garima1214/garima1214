fe035737f4d185ace3f71ec4411f3e5e713fa4a7
garima1214 <48708539+garima1214@users.noreply.github.com>
2023-12-28 16:40:50 +0530

    Time: 195 ms (5.30%) | Memory: 41.1 MB (20.84%) - LeetSync

---
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> v;
+        for(int i=1;i<arr.size();i++)
+        {
+            arr[i]= arr[i]^arr[i-1];
+        }
+        for(auto i:queries)
+        {
+            int start = i[0];
+            int end =i[1];
+            if(start==0)
+            v.push_back(arr[end]);
+            else
+            v.push_back(arr[start-1]^arr[end]);
+
+        }
+        return v;
+    }
+};
